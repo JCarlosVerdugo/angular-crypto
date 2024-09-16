2d73938a863f6db3483a39b51e19ea01676e2b4d
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-16 16:40:12 -0700

    Done project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+export interface Markets {
+  data:      Coin[];
+  timestamp: number;
+}
+
+
+export interface Coin {
+  exchangeId:            string;
+  rank:                  string;
+  baseSymbol:            string;
+  baseId:                string;
+  quoteSymbol:           string;
+  quoteId:               string;
+  priceQuote:            string;
+  priceUsd:              string;
+  volumeUsd24Hr:         string;
+  percentExchangeVolume: string;
+  tradesCount24Hr:       string;
+  updated:               number;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +30,43 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-crypto';
+
+  coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume',
+  ];
+  searchText = '';
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+
+  ngOnInit() {
+    this.http.get<Markets>('https://api.coincap.io/v2/markets')
+      .subscribe({
+        next: (res) => {
+          this.coins = res.data;
+          this.filteredCoins = res.data;
+        },
+        error: (err) => console.error(err)
+      })
+  }
+
+  Number(text: string) {
+    return Number(text);
+  }
+
+  searchCoin() {
+    this.coins = this.filteredCoins.filter(coin => 
+      coin.baseId.toLowerCase().trim().includes(this.searchText) ||
+      coin.baseSymbol.toLowerCase().trim().includes(this.searchText) 
+    );
+  }
+  
 }
